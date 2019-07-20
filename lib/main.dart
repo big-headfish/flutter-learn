cddfd9dae194716f19dca09a585587e9956a0403
947261129@qq.com <wyj3047981>
2019-07-20 13:02:48 +0800

    路由

--- lib/main.dart
+++ lib/main.dart
@@ -38,12 +38,12 @@ class RandomWordsState extends State<RandomWords> {
       // 注意，在小屏幕上，分割线看起来可能比较吃力。
       itemBuilder: (context, i) {
         // 在每一列之前，添加一个1像素高的分隔线widget
-        if(i.isOdd) return new Divider();
+        if (i.isOdd) return new Divider();
         // 语法 "i ~/ 2" 表示i除以2，但返回值是整形（向下取整），比如i为：1, 2, 3, 4, 5
         // 时，结果为0, 1, 1, 2, 2， 这可以计算出ListView中减去分隔线后的实际单词对数量
         final index = i ~/ 2;
         // 如果是建议列表中最后一个单词对
-        if (index >= _suggestions.length){
+        if (index >= _suggestions.length) {
           // ...接着再生成10个单词对，然后添加到建议列表
           _suggestions.addAll(generateWordPairs().take(10));
         }
@@ -52,7 +52,7 @@ class RandomWordsState extends State<RandomWords> {
     );
   }
 
-  Widget _buildRow(WordPair pair){
+  Widget _buildRow(WordPair pair) {
     final alreadySaved = _saved.contains(pair);
     return new ListTile(
       title: new Text(
@@ -67,11 +67,41 @@ class RandomWordsState extends State<RandomWords> {
         setState(() {
           if (alreadySaved) {
             _saved.remove(pair);
-          }else{
+          } else {
             _saved.add(pair);
           }
         });
       },
+    );
+  }
+
+  void _pushSaved() {
+    Navigator.of(context).push(
+      new MaterialPageRoute(
+        builder: (context) {
+          final tiles = _saved.map(
+            (pair) {
+              return new ListTile(
+                title: new Text(
+                  pair.asPascalCase,
+                  style: _biggerFont,
+                ),
+              );
+            },
+          );
+          final divided = ListTile.divideTiles(
+            context: context,
+            tiles: tiles,
+          ).toList();
+
+          return new Scaffold(
+            appBar: new AppBar(
+              title: new Text('Save Suggestions'),
+            ),
+            body: new ListView(children: divided,),
+          );
+        },
+      ),
     );
   }
 
@@ -80,11 +110,14 @@ class RandomWordsState extends State<RandomWords> {
     // TODO: implement build
 //    final wordPair = new WordPair.random();
 //    return new Text(wordPair.asPascalCase);
-  return new Scaffold(
-    appBar: new AppBar(
-      title: new Text('Startup Name Generator'),
-    ),
-    body: _buildSuggestions(),
-  );
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('Startup Name Generator'),
+        actions: <Widget>[
+          new IconButton(icon: new Icon(Icons.list), onPressed: _pushSaved),
+        ],
+      ),
+      body: _buildSuggestions(),
+    );
   }
 }
